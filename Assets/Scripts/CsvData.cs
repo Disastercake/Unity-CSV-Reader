cfc2d7cef1ce325cfdca49d63a14047c5b5cc4f1
Disastercake <mike@disastercake.com>
2021-06-25 22:54:07 -0400

    Added CsvData.cs
    CsvData will be an optional data structure that contains the parsed CSV data with helper methods to create a more intuitive interface than just working with 2-dimensional arrays.

---
+++ Assets/Scripts/CsvData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csv
+{
+    /// <summary>
+    /// The parsed data read from a CSV file through CsvHandler.
+    /// </summary>
+    public class CsvData
+    {
+        public CsvData() { }
+        public CsvData(List<List<string>> rawData)
+        {
+            RawData = rawData;
+        }
+
+        /// <summary>
+        /// The raw data parsed from the CSV file.
+        /// </summary>
+        public List<List<string>> RawData { get; private set; }
+
+        public void SetRawData(List<List<string>> rawData)
+        {
+            RawData = rawData;
+        }
+    }
+}
