5642450503a7a959fb5b66ae30969258aa515893
Disastercake <mike@disastercake.com>
2021-06-26 15:15:58 -0400

    Create Messages.cs
    Messages.cs will act as a helper class for use with the Messenger system to prevent typos and maintain unique message names.

---
+++ Assets/Scripts/Messenger System/Messages.cs
@@ -0,0 +1,10 @@
+﻿/// <summary>
+/// Helper class to easily track messages used with the Messenger system.
+/// </summary>
+public static class Messages
+{
+    /// <summary>
+    /// Called when a directory for the CSV files has been chosen.
+    /// </summary>
+    public const string DirectoryChosen = "DirectoryChosen";
+}
