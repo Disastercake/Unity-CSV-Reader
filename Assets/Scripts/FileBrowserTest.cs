69c47ba9f49bfaf2cf618641ed18e81c250c9355
Disastercake <mike@disastercake.com>
2021-06-26 13:36:42 -0400

    Update FileBrowserTest.cs
    Now selects a directory rather than a file.

--- Assets/Scripts/FileBrowserTest.cs
+++ Assets/Scripts/FileBrowserTest.cs
@@ -60,7 +60,7 @@ public class FileBrowserTest : MonoBehaviour
 		// Load file/folder: both, Allow multiple selection: true
 		// Initial path: default (Documents), Initial filename: empty
 		// Title: "Load File", Submit button text: "Load"
-		yield return FileBrowser.WaitForLoadDialog(FileBrowser.PickMode.FilesAndFolders, true, null, null, "Load Files and Folders", "Load");
+		yield return FileBrowser.WaitForLoadDialog(FileBrowser.PickMode.Folders, true, null, null, "Load Files and Folders", "Load");
 
 		// Dialog is closed
 		// Print whether the user has selected some files/folders or cancelled the operation (FileBrowser.Success)
