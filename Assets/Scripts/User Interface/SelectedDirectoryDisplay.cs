2a25a49b4c2c87e317e2cf13c6f4e83082097031
Disastercake <mike@disastercake.com>
2021-06-26 15:25:05 -0400

    Create SelectedDirectoryDisplay.cs
    Created a script that utilizes the messenger system to display the most recently selected directory.

---
+++ Assets/Scripts/User Interface/SelectedDirectoryDisplay.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Listens for Messages.DirectoryChosen 
+/// </summary>
+[DisallowMultipleComponent]
+public class SelectedDirectoryDisplay : MonoBehaviour
+{
+    private TMPro.TextMeshProUGUI _textComp = null;
+    private void Awake()
+    {
+        _textComp = GetComponent<TMPro.TextMeshProUGUI>();
+        _textComp.text = string.Empty;
+
+        try { Messenger<string>.AddListener(Messages.DirectoryChosen, DirectoryChosen); } catch { }
+    }
+
+    private void OnDestroy()
+    {
+        try { Messenger<string>.RemoveListener(Messages.DirectoryChosen, DirectoryChosen); } catch { }
+    }
+
+    private void DirectoryChosen(string directory)
+    {
+        _textComp.text = directory;
+    }
+}
