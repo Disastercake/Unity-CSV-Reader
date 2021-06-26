5254655ce56ca4ef2792ac32b6102f917ec7fc68
Disastercake <mike@disastercake.com>
2021-06-25 23:39:08 -0400

    Update CsvData.cs
    Added helper methods for getting a row or column.

--- Assets/Scripts/CsvData.cs
+++ Assets/Scripts/CsvData.cs
@@ -26,5 +26,50 @@ namespace Csv
         {
             RawData = rawData;
         }
+
+        /// <summary>
+        /// Rows start with an index of 0.
+        /// Example: What would normall be row 1 in a spreadsheet viewer would be 0 in this method.
+        /// 2 in a spreadsheet would be 1 in this method.
+        /// </summary>
+        public bool TryGetRow(int index, out List<string> data)
+        {
+            data = null;
+
+            if (RawData == null) return false;
+            if (RawData.Count-1 < index) return false;
+
+            data = RawData[index];
+
+            return data != null;
+        }
+
+        /// <summary>
+        /// Creates a new list with every value in the specified column.
+        /// </summary>
+        public bool TryGetColumn(int index, bool includeHeader, out List<string> data)
+        {
+            data = null;
+
+            if (RawData == null) return false;
+            if (RawData.Count <= 0) return false;
+            if (RawData[0].Count - 1 < index) return false;
+
+            data = new List<string>(RawData[0].Count);
+
+            for (int i = 0; i < RawData[0].Count-1; i++)
+            {
+                if (i == 0 && !includeHeader)
+                    continue;
+
+                if (RawData[i] != null)
+                    if (RawData[i].Count > index)
+                        data.Add(RawData[i][index]);
+            }
+
+            data = RawData[index];
+
+            return data.Count > 0;
+        }
     }
 }
