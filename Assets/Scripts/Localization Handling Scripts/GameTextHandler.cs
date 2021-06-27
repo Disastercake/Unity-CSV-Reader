fb80a7641162906a3e4eb5f95bb90cf0ae6890fc
Disastercake <mike@disastercake.com>
2021-06-26 20:51:59 -0400

    Create GameTextHander.cs

---
+++ Assets/Scripts/Localization Handling Scripts/GameTextHandler.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTextHandler : MonoBehaviour
+{
+    private TMPro.TextMeshProUGUI _textComp = null;
+
+    private void Awake()
+    {
+        _textComp = GetComponent<TMPro.TextMeshProUGUI>();
+    }
+
+    void Start()
+    {
+        try { Messenger.AddListener(Messages.LocSettingChanged, DisplayText); } catch { }
+        DisplayText();
+    }
+
+    private void OnDestroy()
+    {
+        try { Messenger.RemoveListener(Messages.LocSettingChanged, DisplayText); } catch { }
+    }
+
+    private void DisplayText()
+    {
+        string txt = string.Empty;
+
+        Csv.CsvData data = new Csv.CsvData();
+
+        if (Csv.CsvHandler.TryGetCsvDataFromPath(LocalizationHandler.CurrentLocFilePath(), in data))
+        {
+            List<string> row;
+
+            if (data.TryGetRow(0, out row))
+                txt = row[0];
+            else
+                Debug.LogError("TryGetRow");
+        }
+        else
+        {
+            Debug.LogError("TryGetCsvDataFromPath: " + LocalizationHandler.CurrentLocFilePath());
+        }
+
+        _textComp.text = txt;
+    }
+}
